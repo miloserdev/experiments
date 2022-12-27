337bb069f82127f4ee8666e3c5fd3baf87ef0ec5
Max <37951044+miloserdev@users.noreply.github.com>
2022-12-27 14:52:50 +0300

    Update esp32_relay.ino

--- esp32_relay/esp32_relay.ino
+++ esp32_relay/esp32_relay.ino
@@ -1,14 +1,22 @@
-#define R1 4
-#define R2 0
-#define R3 2
-#define R4 15
+#define R1 32
+#define R2 33
+#define R3 25
+#define R4 26
+#define R5 27
 
-#include "RTCDS1307.h"
+// #include "RTCDS1307.h"
 #include "WiFi.h"
 #include "AsyncUDP.h"
 #include "JSON.h"
 
-RTCDS1307 rtc(0x68);
+#include <SD.h>
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 RTC;
+
+DateTime _now;
+
+//RTCDS1307 rtc(0x68);
 
 AsyncUDP udp;
 const char* ssid = "********";
@@ -25,27 +33,35 @@ uint8_t year, month, weekday, day, hour, minute, second;
 bool period = 0;
 
 void get_time() {
-    rtc.getDate(year, month, day, weekday);
-    rtc.getTime(hour, minute, second, period);
+    char row[128];
+    _now = RTC.now();
+    Serial.print("print=");
+    Serial.println(_now.unixtime());
+    snprintf(row,sizeof(row), "printf=%lu", _now.unixtime());
+    Serial.println(row);
 }
 
 
 void setup() {
 
-    rtc.begin();
+//    rtc.begin();
+    Wire.begin();
+    RTC.begin();
     get_time();
 
     pinMode(R1, OUTPUT);
     pinMode(R2, OUTPUT);
     pinMode(R3, OUTPUT);
     pinMode(R4, OUTPUT);
+    pinMode(R5, OUTPUT);
 
     digitalWrite(R1, HIGH);
     digitalWrite(R2, HIGH);
     digitalWrite(R3, HIGH);
     digitalWrite(R4, HIGH);
+    digitalWrite(R5, HIGH);
 
-    pinMode(13, INPUT);
+//    pinMode(13, INPUT); //thermal;
 
 
     Serial.begin(115200);
@@ -77,13 +93,16 @@ void setup() {
         Serial.println("UDP connected");
         udp.onPacket([](AsyncUDPPacket packet) {
             //reply to the client
-            packet.printf("Got %u bytes of data", packet.length());
+        //    packet.printf("Got %u bytes of data", packet.length());
 
             buffer = JSON.parse( (char*) packet.data());
 
             Serial.println(buffer);
 
-            parse_packet(buffer, packet);
+            char *ret = exec_packet(buffer);
+            packet.print( ret);
+
+//            parse_packet(buffer, packet);
 
 
         });
@@ -95,12 +114,23 @@ void setup() {
 
 void loop() {
     exec_scheduler();
+//    print_time();
 }
 
+/*
+int prttm = 0;
+void print_time () {
+    int now = _now.unixtime();
+    if (now > prttm) {
+        prttm = now + 5;
+        Serial.println(now);
+    }
+}
+*/
 
 
 
-char* parse_packet(JSONVar data, AsyncUDPPacket packet) {
+char* parse_packet(JSONVar &data, AsyncUDPPacket &packet) {
     Serial.println("parse packet");
     char *ret = exec_packet(data);
     packet.print( ret);
@@ -124,7 +154,7 @@ void exec_scheduler() {
 }
 
 
-void scheduled(JSONVar command/*uint8_t command[128], size_t sz*/, int next_time) {
+void scheduled(JSONVar &command, int next_time) {
     int current_time = millis();
     scheduled_time = current_time + next_time;
     Serial.println(command);
@@ -146,69 +176,91 @@ void scheduled_clear() {
 }
 
 
-char * exec_packet(JSONVar data) {
+char * exec_packet(JSONVar &pack) {
     Serial.println("exec packet begin");
 
-//    Serial.println(data["pin"]);
-    Serial.println( data.hasOwnProperty("schedule") );
-    Serial.println( data.hasOwnProperty("pin") );
+    if (JSON.typeof(pack) == "undefined") {
+        Serial.println("parser malfunction");
+        return "parser malfunction"; 
+    }
 
-    if ( data.hasOwnProperty("schedule") ) {
-        Serial.println("schedule");
+    if (pack.length() < 0) {
+        return "data misfunction";
+    }
 
-        if ( JSON.typeof(data["schedule"]) == String("string") ) {
-            Serial.println("is string");
+    char *ret_ = "";
 
-            if ( data.hasPropertyEqual("schedule", "state") ) {
-                Serial.println("getting state schedule");
-                return "scheduler state";
+    Serial.print(pack.length());
+
+    for (uint32_t i = 0; i < pack.length(); i++) {
+        JSONVar data = pack[i];
+        Serial.println(data);
+    //    Serial.println(data["pin"]);
+    //    Serial.println( data.hasOwnProperty("schedule") );
+    //    Serial.println( data.hasOwnProperty("pin") );
+
+        if ( data.hasOwnProperty("schedule") ) {
+            Serial.println("schedule");
+
+            if ( JSON.typeof(data["schedule"]) == String("string") ) {
+                Serial.println("is string");
+
+                if ( data.hasPropertyEqual("schedule", "state") ) {
+                    Serial.println("getting state schedule");
+                    ret_ = (char*) (scheduled_isset ? "scheduled" : "not scheduled");
+                    continue;
+                }
+
+                if ( data.hasPropertyEqual("schedule", "clear") ) {
+                    scheduled_clear();
+                    ret_ = "ok; scheduler is clear";
+                    continue;
+                }
+
+            } else if ( JSON.typeof(data["schedule"]) == String("object") ) {
+
+                if ( JSONVar(data["schedule"]).hasOwnProperty("time") &&
+                    JSONVar(data["schedule"]).hasOwnProperty("cmd") ) {
+                        
+                        int tme = data["schedule"]["time"];
+                        JSONVar cmd = data["schedule"]["cmd"];
+                        scheduled(cmd, tme);
+
+                        ret_ = "ok; scheduler is set";
+                        continue;
+                } else {
+                    return "error; no time or cmd parameter";
+                }
             }
 
-            if ( data.hasPropertyEqual("schedule", "clear") ) {
-                scheduled_clear();
-                return "scheduler is clear";
-            }
-
-        } else if ( JSON.typeof(data["schedule"]) == String("object") ) {
-
-            if ( JSONVar(data["schedule"]).hasOwnProperty("time") &&
-                 JSONVar(data["schedule"]).hasOwnProperty("cmd") ) {
-                    
-                    int tme = data["schedule"]["time"];
-                    JSONVar cmd = data["schedule"]["cmd"];
-                    scheduled(cmd, tme);
-
-                    return "scheduler is set";
-            } else {
-                return "no time or cmd parameter";
-            }
         }
 
+        if ( data.hasOwnProperty("digitalWrite") ) {
+            if ( JSONVar(data["digitalWrite"]).hasOwnProperty("pin") &&
+                JSONVar(data["digitalWrite"]).hasOwnProperty("value") ) {
+                                        
+                    int pin = data["digitalWrite"]["pin"];
+                    int val = data["digitalWrite"]["value"];
+                    digitalWrite(pin, val);
+
+                    Serial.println("digitalWrite");
+                    ret_ = "ok; digitalWrite";
+                    continue;
+                }
+        }
+
+        if ( data.hasOwnProperty("digitalRead") ) {
+            if ( JSONVar(data["digitalRead"]).hasOwnProperty("pin") ) {
+                                        
+                    int pin = data["digitalRead"]["pin"];
+                    int val = digitalRead(pin);
+                    ret_ = (char*) (val ? "off" : "on");
+
+                    Serial.println("digitalRead");
+                    continue;
+                }
+        }
     }
-
-    if ( data.hasOwnProperty("digitalWrite") ) {
-        if ( JSONVar(data["digitalWrite"]).hasOwnProperty("pin") &&
-            JSONVar(data["digitalWrite"]).hasOwnProperty("value") ) {
-                                    
-                int pin = data["digitalWrite"]["pin"];
-                int val = data["digitalWrite"]["value"];
-                digitalWrite(pin, val);
-
-                Serial.println("digitalWrite");
-                return "digitalWrite";
-            }
-    }
-
-    if ( data.hasOwnProperty("digitalRead") ) {
-        if ( JSONVar(data["digitalRead"]).hasOwnProperty("pin") ) {
-                                    
-                int pin = data["digitalRead"]["pin"];
-                int val = digitalRead(pin);
-
-                Serial.println("digitalRead");
-                return (char*) val;
-            }
-    }
+    return ret_;
 }
-
 
