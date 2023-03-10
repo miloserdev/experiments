ab278dca993128ffc6dd75ead18c1600a8fff7c6
miloserdev <themarshallelite@gmail.com>
2023-03-10 19:35:08 +0300

    null

--- stack_example/main.c
+++ stack_example/main.c
@@ -10,14 +10,12 @@ uint16_t stack[STACK_SIZE] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x99 };
 
 
 
-bool stack_print()
+void stack_print()
 {
-
 	for (size_t i = 0; i < STACK_SIZE; i++)
 	{
 		printf("%d -> 0x%04X \n", i, stack[i]);
 	}
-	return true;
 }
 
 
@@ -41,6 +39,15 @@ bool stack_push(uint16_t *dest, uint16_t *data)
 	return true;
 }
 
+bool stack_exists(uint16_t *stack, uint16_t *data)
+{
+	for (size_t i = 0; i < STACK_SIZE; i++)
+	{
+		if (stack[i] == data) return true;
+	}
+	return false;
+}
+
 
 int main ()
 {
@@ -56,4 +63,13 @@ int main ()
 
 	printf("___ after ___ \n");
 	stack_print();
+
+
+	uint16_t data = 0xCE;
+	bool exists = stack_exists(stack, data);
+	printf("___ is 0x%04X %s exist ___ \n", data, (exists ? "is" : "does not") );
+
+	data = 0xCC;
+	exists = stack_exists(stack, data);
+	printf("___ is 0x%04X %s exist ___ \n", data, (exists ? "is" : "does not") );
 }
